956da47d718c47a56596e19b71cb5ddbb4c67062
dimas.rodriguez002 <dimas.rodriguez002@2133-B2D-343538.mykendall.mymdc.net>
2017-04-11 18:27:27 -0400

    Learning How to use GIT and Git hub, the code merely prints a single to the public

---
+++ src/hello/world/pkg2/HelloWorld2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world.pkg2;
+
+/**
+ *
+ * @author dimas.rodriguez002
+ */
+public class HelloWorld2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("welcome to java program!!!");
+    }
+    
+}
